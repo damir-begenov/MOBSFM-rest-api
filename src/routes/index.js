816d9135c385848c60acf5ecdb2e9cdf4808aa76
Damir <damir.ps04@gmail.com>
2023-07-12 16:01:27 +0600

    fdfdsf

--- src/routes/index.js
+++ src/routes/index.js
@@ -22,7 +22,7 @@ router.get('/api', (req, res) => {
 
 router.get('/education', (req, res) => {
     db.task(async t => {
-        const education_category = await t.manyOrNone('SELECT * FROM education_educationmaterial')
+        const education_category = await t.manyOrNone('SELECT * FROM education_educationmaterial INNER JOIN education_educationfile ON education_educationmaterial.id = education_educationfile.category_id')
         res.json({
             education: education_category
         })
