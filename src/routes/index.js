5258b05c405d44ca3478ad4db15a04b38a107baa
Damir <damir.ps04@gmail.com>
2023-07-13 13:25:17 +0600

    fdfdsf

--- src/routes/index.js
+++ src/routes/index.js
@@ -23,7 +23,7 @@ router.get('/api', (req, res) => {
 router.get('/education', (req, res) => {
     db.task(async t => {
         const education_category = await t.manyOrNone('SELECT * FROM education_category');
-        const education_category_material = await t.manyOrNone('SELECT ec.name as nameaac, ec.name_kk as nameaac_kk, eem.* FROM education_educationmaterial AS eem INNER JOIN education_educationfile AS eef ON eem.id = eef.material_id INNER JOIN education_category AS ec ON eem.category_id = ec.id')
+        const education_category_material = await t.manyOrNone('SELECT ec.name as nameaac, ec.name_kk as nameaac_kk, eem.* , eef.file as filee FROM education_educationmaterial AS eem INNER JOIN education_educationfile AS eef ON eem.id = eef.material_id INNER JOIN education_category AS ec ON eem.category_id = ec.id')
         
         res.json({
             education_category: education_category,
