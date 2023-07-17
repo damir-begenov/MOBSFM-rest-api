1ae9226a25415afd929b63cba9f3bc6a0f38f01d
kuanysh-rgb <kuanysh01012@gmail.com>
2023-07-17 17:46:21 +0600

    IO

--- src/routes/index.js
+++ src/routes/index.js
@@ -272,7 +272,7 @@ router.post('/login', (req, res) => {
                 const subjectCode = await t.oneOrNone('SELECT name FROM directories_codetype WHERE id = $1', [cfmCode]);
 
                 const orgType = await t.oneOrNone('SELECT type FROM accounts_organization WHERE iin = $1', [iin]);
-                const userRole = await t.many('SELECT role FROM accounts_employee WHERE client_user_id = $1', [userId]);
+                const userRole = await t.many('SELECT role FROM accounts_employee WHERE client_user_id = $1', [user.id]);
                 org_address = await t.oneOrNone('SELECT * FROM accounts_organizationaddres WHERE organization_id = $1', [organization['id']]);
                 if(org_address!=null){
                     org_country = await t.oneOrNone('SELECT * FROM directories_country WHERE id = $1', [org_address['country_id']]);
