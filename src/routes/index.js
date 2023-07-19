3c4725385c18e02962a52a3f297d0330d86988bf
Damir <damir.ps04@gmail.com>
2023-07-19 16:49:28 +0600

    faq

--- src/routes/index.js
+++ src/routes/index.js
@@ -138,14 +138,15 @@ router.post('/assessment', (req, res) => {
           GROUP BY assessments_assessmentitemcategory.code;`, [organization_id]);
           assessment_qualification_sum[0]['category_code'] = 'Квалификация';
           const all_points = await t.manyOrNone(`SELECT
-          SUM(assessments_assessmentitem.point) AS total_points
+          SUM(assessments_assessmentitem.point) AS total_points, assessments_grade.color
         FROM assessments_assessment
         INNER JOIN assessments_assessmentitem ON assessments_assessment.id = assessments_assessmentitem.assessment_id
         INNER JOIN assessments_assessmentitemcode ON assessments_assessmentitemcode.id = assessments_assessmentitem.code_id
         INNER JOIN assessments_assessmentitemcategory ON assessments_assessmentitemcategory.id = assessments_assessmentitemcode.category_id
+        INNER JOIN assessments_grade ON assessments_assessmentitemcategory.id = assessments_grade.item_category_id
         WHERE assessments_assessment.date >= date_trunc('month', current_date) + INTERVAL '1 day'
           AND assessments_assessment.date < (date_trunc('month', current_date) + INTERVAL '1 month' - INTERVAL '1 day')
-          AND assessments_assessment.organization_id = $1;`, [organization_id]);
+          AND assessments_assessment.organization_id = $1 group by assessments_grade.color;`, [organization_id]);
           //   const all_points = assessment_qualification_sum[0]['total_points'] + assessment_fin[0]['total_points'] + assessment_regulator_documents[0]['total_points'] + assessment_main_info[0]['total_points'];
         res.json({
             assessments: assessments,
