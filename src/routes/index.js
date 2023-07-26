a72f5bbda46b29267bf926d4980bc703cdb8d50a
Damir <damir.ps04@gmail.com>
2023-07-26 11:14:12 +0600

    faq

--- src/routes/index.js
+++ src/routes/index.js
@@ -146,15 +146,30 @@ router.post('/assessment', (req, res) => {
         WHERE assessments_assessment.date >= date_trunc('month', current_date) + INTERVAL '1 day'
           AND assessments_assessment.date < (date_trunc('month', current_date) + INTERVAL '1 month' - INTERVAL '1 day')
           AND assessments_assessment.organization_id = $1 ;`, [organization_id]);
-          const all_points_2 = await t.manyOrNone(`SELECT
-          SUM(assessments_assessmentitem.point) AS total_points
-        FROM assessments_assessment
-        INNER JOIN assessments_assessmentitem ON assessments_assessment.id = assessments_assessmentitem.assessment_id
-        INNER JOIN assessments_assessmentitemcode ON assessments_assessmentitemcode.id = assessments_assessmentitem.code_id
-        INNER JOIN assessments_assessmentitemcategory ON assessments_assessmentitemcategory.id = assessments_assessmentitemcode.category_id
-        WHERE assessments_assessment.date <= date_trunc('month', current_date) - INTERVAL '1 month' + INTERVAL '1 day'
-          AND assessments_assessment.date > (date_trunc('month', current_date) - INTERVAL '2 month' - INTERVAL '1 day')
-          AND assessments_assessment.organization_id = $1 ;`, [organization_id]);
+          const all_points_2 = await t.manyOrNone(`WITH RECURSIVE months AS (
+            SELECT date_trunc('month', MIN(assessments_assessment.date)) AS month
+            FROM assessments_assessment
+            WHERE assessments_assessment.organization_id = $1
+            UNION ALL
+            SELECT month + INTERVAL '1 month'
+            FROM months
+            WHERE month < date_trunc('month', current_date)
+          )
+          SELECT
+            months.month,
+            SUM(assessments_assessmentitem.point) AS total_points
+          FROM
+            months
+          LEFT JOIN assessments_assessment ON date_trunc('month', assessments_assessment.date) = months.month
+          LEFT JOIN assessments_assessmentitem ON assessments_assessment.id = assessments_assessmentitem.assessment_id
+          LEFT JOIN assessments_assessmentitemcode ON assessments_assessmentitemcode.id = assessments_assessmentitem.code_id
+          LEFT JOIN assessments_assessmentitemcategory ON assessments_assessmentitemcategory.id = assessments_assessmentitemcode.category_id
+          WHERE
+            assessments_assessment.organization_id = $1
+          GROUP BY
+            months.month
+          ORDER BY
+            months.month;`, [organization_id]);
           //   const all_points = assessment_qualification_sum[0]['total_points'] + assessment_fin[0]['total_points'] + assessment_regulator_documents[0]['total_points'] + assessment_main_info[0]['total_points'];
         res.json({
             assessments: assessments,
@@ -166,7 +181,7 @@ router.post('/assessment', (req, res) => {
             assessment_qualification_sum: assessment_qualification_sum,
             total_points: all_points,
             total_points_2 : all_points_2
-        })
+        }) 
     });
 });
 
