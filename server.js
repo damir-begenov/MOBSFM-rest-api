bbd202f4f09ca33eae33f90833fc4d754e48233d
kuanysh-rgb <kuanysh01012@gmail.com>
2023-07-26 09:22:16 +0600

    IO

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 
  const app = require('./src/app');
 
- const port = process.env.PORT || 3000;
+ const port = process.env.PORT;
  
  app.listen(port, () => {
    console.log('Приложение запущена на порту ', port);
