f44aaa1c981b0da4fbb5a9ccf62ae64030251851
Damir <damir.ps04@gmail.com>
2023-08-07 13:51:55 +0600

    faq

--- src/routes/index.js
+++ src/routes/index.js
@@ -264,7 +264,7 @@ router.post('/ohvat', (req,res) => {
         for (var i = 0; i < length; i++) {
             // Do something with 'item', which represents each element of the array
                 const codetype = await t.manyOrNone(`SELECT * FROM directories_codetype
-                                                     where code = $1`, [fff[i]]);
+                                                     where id = $1`, [fff[i]]);
                 const organization_ohvat = await t.manyOrNone(`SELECT count(*) FROM accounts_organization
                                                                where subject_code_id = $1 and status = 'approved'`, [codetype[0]['id']]);
                 codetype[0]['countapproved'] = parseFloat(organization_ohvat[0]['count']);
