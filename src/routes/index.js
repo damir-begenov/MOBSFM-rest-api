0ea5b9a15d03b4686b7f43185c840a59c878ff37
kuanysh-rgb <kuanysh01012@gmail.com>
2023-08-08 12:42:23 +0600

    IO

--- src/routes/index.js
+++ src/routes/index.js
@@ -59,24 +59,24 @@ router.post('/ocenkaBVU', (req, res) => {
     db.task(async t => {
         const idshka = await t.manyOrNone(`SELECT id FROM public.assessments_bankassessment ab where "date" >= date_trunc('month', current_date) + INTERVAL '1 day'
         AND "date" < (date_trunc('month', current_date) + INTERVAL '1 month' - INTERVAL '1 day') and organization_id = $1`, [organization_id]);
-        const first = await t.manyOrNone(`select sum(point)/count(*) from public.assessments_bankinteractionlevel ab where assessment_id in
+        const bankinteractionlevel = await t.manyOrNone(`select sum(point)/count(*) as bankinteractionlevel from public.assessments_bankinteractionlevel ab where assessment_id in
          (SELECT id FROM public.assessments_bankassessment ab where "date" >= date_trunc('month', current_date) + INTERVAL '1 day'
          AND "date" < (date_trunc('month', current_date) + INTERVAL '1 month' - INTERVAL '1 day') and organization_id = $1)`, [organization_id]);
-        const second = await t.manyOrNone(`select sum(point)/count(*) from public.assessments_suspensionquality ab where assessment_id in (SELECT id FROM public.assessments_bankassessment ab where "date" >= date_trunc('month', current_date) + INTERVAL '1 day'
+        const suspensionquality = await t.manyOrNone(`select sum(point)/count(*) as suspensionquality from public.assessments_suspensionquality ab where assessment_id in (SELECT id FROM public.assessments_bankassessment ab where "date" >= date_trunc('month', current_date) + INTERVAL '1 day'
         AND "date" < (date_trunc('month', current_date) + INTERVAL '1 month' - INTERVAL '1 day') and organization_id = $1)`, [organization_id]);
-        const third = await t.manyOrNone(`select sum(point)/count(*) from public.assessments_sentmessagescorrectness where assessment_id in (SELECT id FROM public.assessments_bankassessment ab where "date" >= date_trunc('month', current_date) + INTERVAL '1 day'
+        const sentmessagescorrectness = await t.manyOrNone(`select sum(point)/count(*) as sentmessagescorrectness from public.assessments_sentmessagescorrectness where assessment_id in (SELECT id FROM public.assessments_bankassessment ab where "date" >= date_trunc('month', current_date) + INTERVAL '1 day'
         AND "date" < (date_trunc('month', current_date) + INTERVAL '1 month' - INTERVAL '1 day')  and organization_id = $1)`, [organization_id]);
-        const fourth = await t.manyOrNone(`select sum(point)/count(*) from public.assessments_cashingoutbankinvolvement where assessment_id in (SELECT id FROM public.assessments_bankassessment ab where "date" >= date_trunc('month', current_date) + INTERVAL '1 day'
+        const cashingoutbankinvolvement = await t.manyOrNone(`select sum(point)/count(*) as cashingoutbankinvolvement from public.assessments_cashingoutbankinvolvement where assessment_id in (SELECT id FROM public.assessments_bankassessment ab where "date" >= date_trunc('month', current_date) + INTERVAL '1 day'
         AND "date" < (date_trunc('month', current_date) + INTERVAL '1 month' - INTERVAL '1 day')  and organization_id = $1)`, [organization_id]);
-        const fifth = await t.manyOrNone(`select sum(point)/count(*) from public.assessments_internalrulesapplication where assessment_id in (SELECT id FROM public.assessments_bankassessment  ab where "date" >= date_trunc('month', current_date) + INTERVAL '1 day'
+        const internalrulesapplication = await t.manyOrNone(`select sum(point)/count(*) as internalrulesapplication from public.assessments_internalrulesapplication where assessment_id in (SELECT id FROM public.assessments_bankassessment  ab where "date" >= date_trunc('month', current_date) + INTERVAL '1 day'
         AND "date" < (date_trunc('month', current_date) + INTERVAL '1 month' - INTERVAL '1 day') and organization_id = $1)`, [organization_id]);
         const results = {
             idshka: idshka,
-            bankinteractionlevel: first,
-            suspensionquality: second,
-            sentmessagescorrectness: third,
-            cashingoutbankinvolvement: fourth,
-            internalrulesapplication: fifth,
+            bankinteractionlevel: bankinteractionlevel,
+            suspensionquality: suspensionquality,
+            sentmessagescorrectness: sentmessagescorrectness,
+            cashingoutbankinvolvement: cashingoutbankinvolvement,
+            internalrulesapplication: internalrulesapplication,
         };
         res.json({
             results: results
