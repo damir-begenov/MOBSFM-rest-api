9332651c8a6c59d9dc87f1aa4097dfed942f7530
Damir <damir.ps04@gmail.com>
2023-08-08 15:54:50 +0600

    faq

--- src/routes/index.js
+++ src/routes/index.js
@@ -349,12 +349,10 @@ router.post('/vovlechennost', (req,res) => {
                                                                     GROUP BY organization_id, sess.date) AS ass 
                                                                 where ass.ass_points>2 and ass.ass_points<=24 
                                                                 `,[subject_code_id[i]]); 
-                console.log(vovlechennost[0]['count']);    
                 codetype[0]['countapproved'] = parseFloat(organization_ohvat[0]['count']);
-                codetype[0]['procents_of_org_names'] = (organization_ohvat[0]['count']*100)/parseFloat(codetype[0]['count']);
+                codetype[0]['procents_of_org_names'] = (vovlechennost[0]['count']*100)/parseFloat(codetype[0]['count']);
                 percentage += codetype[0]['procents_of_org_names'];
-                console.log(organization_ohvat[0]['count']);
-                console.log(codetype[0]['count']);
+                organization_ohvat[0]['count'] = vovlechennost[0]['count'];
                 code_types.push(codetype[0]);
                 organization_ohvat_accepted.push(organization_ohvat);
                 if (codetype.length === 0) {
