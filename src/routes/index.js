958afae6f424ee0c2094e4752e05cc7a3b59919f
kuanysh-rgb <kuanysh01012@gmail.com>
2024-01-15 17:18:03 +0600

    Fix received messages

--- src/routes/index.js
+++ src/routes/index.js
@@ -656,10 +656,10 @@ router.post('/getViolations', verifyToken, (req, res) => {
 router.post('/section3Acategory', (req, res) => {
     const {category} = req.body;
     db.task(async t => {
-        const category_content = await t.many('SELECT * FROM sanctions_sanctionterrorist WHERE category = $1', [category])
+        const category_content_acting = await t.many('SELECT * FROM sanctions_sanctionterrorist WHERE category = $1 and status = "acting"', [category])
 
         res.json({
-            category_content: category_content
+            category_content: category_content_acting
         })
     }).catch(error => {
         res.status(500).json({ success: false, message: 'Internal server error', error: error});
